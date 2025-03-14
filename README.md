07e23809fcaf929f6219efe222187ea838a715b5
chenqiang <798714038@qq.com>
2025-03-14 09:14:23 +0800

    tcp server

---
+++ README.md
@@ -0,0 +1,8 @@
+# 一个基于Kameo的分布式游戏服务器框架
+
+## 技术选型
+- KeyDB  数据库冷热数据
+- Cap'n  Proto 数据协议 存储,通讯
+- Kameo 分布式
+- Luban  配置
+- Garage 配置热更
--- common/src/network/mod.rs
+++ common/src/network/mod.rs
@@ -1,19 +1,50 @@
-use bytes::Bytes;
+mod tcp;
 
-pub struct Msg {
+use crate::network::tcp::start_tcp_server;
+use bytes::Bytes;
+use std::net::SocketAddr;
+use kameo::message::{Context, Message};
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpStream;
+use tokio::sync::mpsc::channel;
+
+pub struct LogicMessage {
     pub ix: usize,
     pub cmd: u16,
     pub bytes: Bytes,
 }
 
-pub trait Dispatcher {
-    async fn frame_ready(&self, msg: Msg) -> anyhow::Result<Msg>;
+#[derive(Serialize,Deserialize)]
+pub enum NetMessage {
+    Read(LogicMessage),
+    Connected(TcpStream, SocketAddr),
+    Err,
+    Close,
 }
 
-pub struct Network {}
+fn start_gate_server() {
+    let (tx, mut rx) = channel::<NetMessage>(100);
 
-impl Dispatcher for Network {
-    async fn frame_ready(&self, msg: Msg) -> anyhow::Result<Msg> {
-        todo!()
+    tokio::spawn(async move { start_tcp_server("0.0.0.0", 2322, tx) });
+
+    tokio::select! {
+       val = rx.recv() => {
+            if let Some(msg) =val{
+                match msg{
+                    NetMessage::Msg(msg)=>{
+                        println!("msg:{}",msg.ix);
+                    },
+                    NetMessage::Connected(stream,addr)=>{
+                        println!("connected:{}",addr);
+                    },
+                    NetMessage::Err=>{
+                        println!("err");
+                    },
+                    NetMessage::Close=>{
+                        println!("close");
+                    },
+                }
+            }
+       }
     }
 }
